99487cc2aa60a2cff9c8073f3b2d9e22be6834b6
rlawnsgh98 <hoho0084@gmail.com>
2022-11-25 12:20:46 +0900

    rlawnsgh
    김준호

--- SelfCheck/app/src/main/java/com/example/selfcheck/SubActivity.java
+++ SelfCheck/app/src/main/java/com/example/selfcheck/SubActivity.java
@@ -9,22 +9,43 @@ import android.widget.TextView;
 
 public class SubActivity extends AppCompatActivity {
     TextView balance;
+    TextView order_cnt;
+    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
 
         balance = findViewById(R.id.balance);
-
+        order_cnt = findViewById(R.id.order_cnt);
+        
         Intent intent = getIntent();
         int ibal = intent.getIntExtra("balance", -1);
-        int saidaCnt = intent.getIntExtra("buySaidaCnt", -1);
         int cokeCnt = intent.getIntExtra("buyCokeCnt", -1);
-        Log.d("log", "sub : "+ibal);
-        Log.d("log", "saidaCnt : " + saidaCnt);
-        Log.d("log", "cokeCnt : " + cokeCnt);
-        balance.setText("잔액 : "+ibal);
+        int saidaCnt = intent.getIntExtra("buySaidaCnt", -1);
+        int fantaCnt = intent.getIntExtra("buyFantaCnt", -1);
+        int demisodaCnt = intent.getIntExtra("buyDemisodaCnt", -1);
 
+        Log.d("log", "sub : "+ibal);
+        Log.d("log", "cokeCnt : " + cokeCnt);
+        Log.d("log", "saidaCnt : " + saidaCnt);
+        Log.d("log", "fantaCnt : " + fantaCnt);
+        Log.d("log", "demisodaCnt : " + demisodaCnt);
+        balance.setText("잔액 : "+ibal);
+        
+        if(cokeCnt > 0){
+            order_cnt.append("콜라"+cokeCnt+"개\t" );
+        }
+        if(saidaCnt > 0){
+            order_cnt.append("사이다"+saidaCnt+"개\t");
+        }
+        if(fantaCnt > 0){
+            order_cnt.append("환타"+fantaCnt+"개\t");
+        }
+        if(demisodaCnt > 0){
+            order_cnt.append("데미소다"+demisodaCnt+"개\t");
+        }
 
     }
 }
